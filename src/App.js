d4d996343218736edf76fb421d7bd2a1b019cf30
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-02-01 17:43:35 +0530

    Implemented hero, details and courses components fucntionalities

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import Hero from "./pages/app/hero/Hero";
 import Nav from "./components/nav/Nav";
 import Courses from "./pages/app/courses/Courses";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Details from "./pages/app/details/Details";
 
 function App() {
+  const routes = createBrowserRouter([
+    {
+      path: "/", element: <Nav />, // Main route rendering the Nav component
+      children: [
+        {
+          path: "/", element: <Hero /> // Child route rendering the Hero component at the root path
+        },
+        {
+          path: "/courses",
+          children: [
+            {
+              index: true, // This is the index route for the /courses path
+              element: <Courses /> // Renders the Courses component when the /courses route is matched and no specific child route is matched
+            },
+            {
+              path: ":courseId",
+              element: <Details /> // Dynamic route rendering the Details component for any courseId (e.g., /courses/123)
+            }
+          ]
+        }
+      ]
+    }
+  ]);
+
+
+
   return (
-    <div className="App">
-      <Nav />
-      <Hero />
-      <Courses />
-    </div>
+    <RouterProvider router={routes} />
   );
 }
 
--- src/pages/app/details/Details.jsx
+++ src/pages/app/details/Details.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import style from "./Details.module.css";
 import coursesData from "../../../data/courses.json";
+import { useParams } from "react-router-dom";
 //Third task: import useParams hook
 
 function Details() {
+  const { courseId } = useParams();
+  const course = coursesData.find((data) => data.id === courseId);
   return (
     //Third Task: get course id and match it with the data
-    
+
     <div className={style.courses_container}>
       <div className={style.card_container}>
         <div className={style.card_image}>
           <div className={style.image_container}>
             {/*Third task: image source here */}
-            <img src="**IMAGE**" alt="icons" />
+            <img src={course.img} alt="icons" />
           </div>
         </div>
         <div className={style.card_content}>
           {/*Third Task:  Title and Description here */}
-          <h1 className={style.card_title}>**TITLE**</h1>
-          <p className={style.card_description}>**DESCRIPTION**</p>
+          <h1 className={style.card_title}>{course.title}</h1>
+          <p className={style.card_description}> {course.description}</p>
         </div>
       </div>
       {/* Task4: create Link to the Learn Page */}
